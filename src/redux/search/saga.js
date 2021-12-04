17abf77667155fc834d6382116e75ae9c378b7e1
doremichin <johnkim994@naver.com>
2021-12-04 16:23:20 +0900

    [YT-19] 메인화면 반응형 레이아웃 에러, 관련영상 로직 에러 수정

--- src/redux/search/saga.js
+++ src/redux/search/saga.js
@@ -1,22 +1,31 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import {
+  takeLatest, call, put, takeEvery,
+} from 'redux-saga/effects';
 
 import { getSearchResultsRest } from '../../api';
 import {
-  getRelatedVideos, getSearchResults, setRelatedVideos, setSearchResults,
+  getNextRelatedVideos, getRelatedVideos, getSearchResults, setNextRelatedVideos, setRelatedVideos, setSearchResults,
 } from './slice';
 
 function* getSearchResultsSaga({ payload }) {
   const result = yield call(getSearchResultsRest, payload);
   yield put(setSearchResults(result));
 }
+
 function* getRelatedVideosSaga({ payload }) {
   const result = yield call(getSearchResultsRest, payload);
   yield put(setRelatedVideos(result));
 }
 
+function* getNextRelatedVideosSaga({ payload }) {
+  const result = yield call(getSearchResultsRest, payload);
+  yield put(setNextRelatedVideos(result));
+}
+
 function* saga() {
   yield takeLatest(getSearchResults.type, getSearchResultsSaga);
-  yield takeLatest(getRelatedVideos.type, getRelatedVideosSaga);
+  yield takeEvery(getRelatedVideos.type, getRelatedVideosSaga);
+  yield takeEvery(getNextRelatedVideos.type, getNextRelatedVideosSaga);
 }
 
 export default saga;
