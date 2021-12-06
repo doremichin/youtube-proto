15875e7537bea419aa676850103a5e87dc943a00
doremichin <johnkim994@naver.com>
2021-12-06 13:15:16 +0900

    [YT-21] Video 페이지 / 연관 동영상 리스트 반응형 로직 수정
    태블릿 사이즈부터 연관영상리스트가 채널정보컴포넌트랑 댓글컴포넌트 사이로 이동해야함intersection observer를 사용하지 않고 버튼을 만들어서 클릭시 데이터 추가로 받아오도록 수정

--- src/views/shared/components/Layout/Layout.styled.js
+++ src/views/shared/components/Layout/Layout.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
 
 export const ContentContainer = styled.div`
   max-width: 1700px;
@@ -9,3 +10,24 @@ export const SectionContainer = styled.div`
   margin: 0 auto;
   max-width: 1500px;
 `;
+
+export const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 1700 });
+  return isDesktop ? children : null;
+};
+export const LaptopOrDesktop = ({ children }) => {
+  const isLaptopOrDesktop = useMediaQuery({ minWidth: 1200 });
+  return isLaptopOrDesktop ? children : null;
+};
+export const Laptop = ({ children }) => {
+  const isLaptop = useMediaQuery({ minWidth: 1200, maxWidth: 1699 });
+  return isLaptop ? children : null;
+};
+export const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 950, maxWidth: 1199 });
+  return isTablet ? children : null;
+};
+export const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 949 });
+  return isMobile ? children : null;
+};
