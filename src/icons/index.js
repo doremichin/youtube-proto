23c96e4a1367eeae55b20bdb58b7c0417605cdba
doremichin <johnkim994@naver.com>
2021-12-08 19:05:23 +0900

    [YT-24] Header / SearchBox 부분 모바일 반응형 개발

--- src/icons/index.js
+++ src/icons/index.js
@@ -628,3 +628,39 @@ className="style-scope yt-icon"
     </g>
   </svg>
 );
+export const IconMicrophone = () => (
+  <svg
+viewBox="0 0 24 24"
+preserveAspectRatio="xMidYMid meet"
+focusable="false"
+className="style-scope yt-icon"
+style={{
+  'pointer-events': 'none', display: 'block', width: '100%', height: '100%',
+}}
+  >
+    <g className="style-scope yt-icon">
+      <path
+            d="M12 3C10.34 3 9 4.37 9 6.07V11.93C9 13.63 10.34 15 12 15C13.66 15 15 13.63 15 11.93V6.07C15 4.37 13.66 3 12 3ZM18.5 12H17.5C17.5 15.03 15.03 17.5 12 17.5C8.97 17.5 6.5 15.03 6.5 12H5.5C5.5 15.24 7.89 17.93 11 18.41V21H13V18.41C16.11 17.93 18.5 15.24 18.5 12Z"
+            className="style-scope yt-icon"
+      />
+    </g>
+  </svg>
+);
+export const IconArrowLeft = () => (
+  <svg
+viewBox="0 0 24 24"
+preserveAspectRatio="xMidYMid meet"
+focusable="false"
+className="style-scope yt-icon"
+style={{
+  'pointer-events': 'none', display: 'block', width: '100%', height: '100%',
+}}
+  >
+    <g mirror-in-rtl="" className="style-scope yt-icon">
+      <path
+d="M21,11v1H5.64l6.72,6.72l-0.71,0.71L3.72,11.5l7.92-7.92l0.71,0.71L5.64,11H21z"
+className="style-scope yt-icon"
+      />
+    </g>
+  </svg>
+);
---
+++ src/views/shared/components/SearchBox/SearchBoxInMobile.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SearchBoxContainer from '../../containers/SearchBoxContainer';
+import { IconArrowLeft, IconMicrophone } from '../../../../icons';
+
+const SearchBoxInMobile = ({ onClick }) => (
+  <Container>
+    <Button onClick={onClick}>
+      <IconArrowLeft />
+    </Button>
+    <Inner>
+      <SearchBoxContainer />
+    </Inner>
+    <Button>
+      <IconMicrophone />
+    </Button>
+  </Container>
+);
+
+const Container = styled.div`
+  display:flex;
+  align-items:center;
+  justify-content: center;
+  position:fixed;
+  z-index: 1000;
+  height: 56px;
+  background: rgba(33, 33, 33, 0.98);
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 0 20px;
+`;
+
+const Inner = styled.div`
+  margin: 0 20px;
+  flex: 1;
+`;
+
+const Button = styled.div`
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  fill: #fff;
+`;
+
+export default SearchBoxInMobile;
