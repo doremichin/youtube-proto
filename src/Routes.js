cf2ae2c8560dd72aa7a675dad16e70d045b95c25
doremichin <johnkim994@naver.com>
2021-12-17 22:46:32 +0900

    [Route] Redirect 기능 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Search from './pages/Search';
@@ -11,6 +11,7 @@ const Routes = () => (
     <Route exact path="/" component={Main} />
     <Route exact path="/results" component={Search} />
     <Route exact path="/watch" component={Video} />
+    <Redirect path="*" push="/" />
   </Switch>
 );
 
